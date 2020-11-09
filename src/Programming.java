17e28e235991219cab93bce6d71fb6ec46c6e9c0
MSAMAK3 <msamak3@ford.com>
2020-11-09 16:34:26 -0500

    Added programming type class

---
+++ src/Programming.java
@@ -0,0 +1,17 @@
+import java.util.stream.IntStream;
+
+public class Programming {
+    public static void main(String[] args) {
+    
+        System.out.println("Imperative Programming");
+        
+        for (int i=0; i<10; i++) {
+            System.out.print(i);
+        }
+    
+        System.out.println("\nDeclarative Programming");
+        IntStream numbers = IntStream.of(1,2,3,4,5);
+        numbers.forEach(System.out::print);
+        
+    }
+}

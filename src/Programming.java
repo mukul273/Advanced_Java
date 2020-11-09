31707dc805d3f3a0227cfee517d9319145dc8e3d
MSAMAK3 <msamak3@ford.com>
2020-11-09 18:56:06 -0500

    Added programming type class

--- src/Programming.java
+++ src/Programming.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Programming {
@@ -9,13 +12,30 @@ public class Programming {
             System.out.print(i);
         }
     
-        System.out.println("\nDeclarative Programming");
+        System.out.println("\nDeclarative Programming example");
         IntStream numbers = IntStream.of(1,2,3,4,5);
         numbers.forEach(System.out::print);
     
-        System.out.println("\nAnother Declarative Programming");
+        System.out.println("\nAnother Declarative Programming example");
         IntStream numbersRange = IntStream.rangeClosed(1,5);
         numbersRange.forEach(System.out::print);
+        
+        System.out.println("\nAnother Declarative Programming example");
+        int numbersSum = IntStream.of(1,2,3,4,5).sum();
+        System.out.println(numbersSum);
+    
+        System.out.println("\nAnother Declarative Programming example");
+        int numbersRangeSum = IntStream.rangeClosed(1,5).sum();
+        System.out.println(numbersRangeSum);
+    
+        System.out.println("\nAnother Declarative Programming example");
+        List<Integer> numbersList= Arrays.asList(2,3,4,5,1);
+        System.out.println(numbersList.stream().min(Integer::compareTo));
+    
+    
+        System.out.println("\nAnother Declarative Programming example");
+        List<String> strList= Arrays.asList("me", "you","no one");
+        System.out.println(strList.stream().sorted().max(String::compareTo));
         
     }
 }

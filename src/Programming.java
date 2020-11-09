81c2e27978af2ab899731f0127b971d6d177d17a
MSAMAK3 <msamak3@ford.com>
2020-11-09 16:37:59 -0500

    Added programming type class

--- src/Programming.java
+++ src/Programming.java
@@ -12,6 +12,10 @@ public class Programming {
         System.out.println("\nDeclarative Programming");
         IntStream numbers = IntStream.of(1,2,3,4,5);
         numbers.forEach(System.out::print);
+    
+        System.out.println("\nAnother Declarative Programming");
+        IntStream numbersRange = IntStream.rangeClosed(1,5);
+        numbersRange.forEach(System.out::print);
         
     }
 }

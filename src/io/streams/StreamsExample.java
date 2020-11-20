f2b4cd71a0a146ffb2b676361bb06c8471ad837e
Muks <msamak@gmail.com>
2020-11-20 15:33:04 -0500

    Added Streams example

--- src/io/streams/StreamsExample.java
+++ src/io/streams/StreamsExample.java
@@ -3,6 +3,8 @@ package io.streams;
 import io.common.Instructor;
 import io.functionalInterfaces.Instructors;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -23,7 +25,23 @@ public class StreamsExample {
 
         System.out.println(mapNamesAndCourses);
 
+        //Use Case - Convert map to ArrayList in various ways
+        System.out.println("Convert MAP to List");
+        Map<String, String> testMap = new HashMap<>();
+        testMap.put("name", "Chimp");
+        testMap.put("address", "USA");
+        testMap.put("phone","000-000-0000");
+
+        List<String> collect1 = testMap.values().stream()
+                .collect(Collectors.toList()); // This is using Stream
+        System.out.println(collect1);
+        ArrayList<String> strings = new ArrayList<>(testMap.keySet()); // This is another way
+        System.out.println(strings);
+
+
+
         //Use Case- ToList
+        System.out.println("ToList");
         List<Instructor> listInstr= instructors.stream()
                 .filter(teachesOnline)
                 .filter(yrsOfExp)
